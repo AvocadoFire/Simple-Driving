e4724cfa25b50ed0a8c6f974207f1d0a20c1ab4d
Laurie <elaurllc@gmail.com>
2021-09-08 20:21:01 -0400

    Android Notification Script

--- Assets/Scripts/AndroidNotifHandler.cs
+++ Assets/Scripts/AndroidNotifHandler.cs
@@ -13,9 +13,23 @@ public class AndroidNotifHandler : MonoBehaviour
         {
             Id = ChannelId, 
             Name = "Notification Channel",
-            Description = "Some random description",
+            Description = "Obstacle Race",
             Importance = Importance.Default
         };
+
+        AndroidNotificationCenter.RegisterNotificationChannel(notificationChannel);
+
+        AndroidNotification notification = new AndroidNotification
+        {
+            Title = "Energy Recharged!",
+            Text = "Your energy has recharged, come back to play again!",
+            SmallIcon = "default",
+            LargeIcon = "default",
+            FireTime = dateTime
+        };
+
+        AndroidNotificationCenter.SendNotification(notification, ChannelId);
+
     }
 
 }
