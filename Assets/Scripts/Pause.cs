10b73a397a270302303da0f09e7598146bd7d72e
Laurie <elaurllc@gmail.com>
2021-09-13 08:34:18 -0400

    Pause button sprite change

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -1,21 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pause : MonoBehaviour
 {
+    [SerializeField] GameObject button;
+    [SerializeField] Sprite buttonImageOriginal;
+    [SerializeField] Sprite buttonImagePaused;
+
+    Image buttonImage;
+    
+    Color buttonColor;
     bool isPaused = false;
+
+    private void Start()
+    {
+       buttonImage = button.GetComponent<Image>();
+    }
+
     public void Paused()
     {
         Debug.Log("entered paused");
         if (isPaused == false)
         {
-        Time.timeScale = 0;
+        buttonImage.sprite = buttonImagePaused;
+            Time.timeScale = 0;
         isPaused = true;
         return;
         }
         Time.timeScale = 1;
         isPaused = false;
-
+        buttonImage.sprite = buttonImageOriginal;
     }
 }
